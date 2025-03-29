02257c45b606545f4743adc27b581eab63089d0e
govinda sah <sahgovinda401@gmail.com>
2025-03-29 14:51:19 +0530

    CI pipeline-4

--- flask_app/app.py
+++ flask_app/app.py
@@ -96,18 +96,18 @@ dagshub.init(
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("nlp_test")
+if not dagshub_token:
+    raise EnvironmentError("nlp_test environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "TechGovinda"
+repo_name = "Review_Sentiment_Analysis"
 # # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
+mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
 # -------------------------------------------------------------------------------------
 
 
